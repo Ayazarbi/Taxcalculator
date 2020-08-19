bceaed5d9774133953bbfbf662a584724003022c
Arbiayaz <Arbiayaz@promactinfo.com>
2020-08-19 14:56:48 +0530

    Made changes in code according to best coding practises Guidelines

--- TaxCalculator-Assignment/ConsoleApp1/Program.cs
+++ TaxCalculator-Assignment/ConsoleApp1/Program.cs
@@ -8,28 +8,48 @@ using System.Threading;
 using System.Transactions;
 using System.Runtime.InteropServices.WindowsRuntime;
 
+
+//class contains all business logic for tax calculations
+
+#region calculator class
 public class TaxCalculator
 {
+
+    //properties Declatratoion section
+    #region Poperties
     private double _userSalary { get; set; }
     private int _userAge { get; set; }
-
     private double _taxAmount { get; set; }
+    #endregion
 
-    private static double _taxChargeforIncomeBetween5to10lacforAgeBelow60 = 12500;
-    private static double _taxChargeforIncomeAbove10lacforAgeBelow60 = 112500;
-    private static double _taxChargeforIncomeBetween5to10lacforAgeBetween60and80 = 10000;
-    private static double _taxChargeforIncomeAbove10lacforAgeBetween60and80 = 110000;
-    private static double _taxChargeforIncomeAbove10lacforAgeAbove80 = 100000;
+    //All the constant required to calculate tax are declared here
+    #region Constant Declaeation
+    private const double _taxChargeforIncomeBetween5to10lacforAgeBelow60 = 12500;
+    private const double _taxChargeforIncomeAbove10lacforAgeBelow60 = 112500;
+    private const double _taxChargeforIncomeBetween5to10lacforAgeBetween60and80 = 10000;
+    private const double _taxChargeforIncomeAbove10lacforAgeBetween60and80 = 110000;
+    private const double _taxChargeforIncomeAbove10lacforAgeAbove80 = 100000;
+    private const int _maxAgeLimit = 100;
+    private const int _ageLimitforfFirstCategory = 60;
+    private const int _ageLimitforSecondgCategory = 80;
+    #endregion
 
+
+
+    //constructor section
+    #region constructor
     public TaxCalculator(double userSalary, int userAge)
     {
         this._userSalary = userSalary;
+
+
+        //this will check valiid user input for constructoe
         if (userAge <= 0 || userSalary <= 0)
         {
             throw new ArgumentException("value cannot be less than or equal to zero");
 
         }
-        else if (userAge > 100)
+        else if (userAge > _maxAgeLimit)
         {
             throw new ArgumentException("User Age cannot be more than 100");
         }
@@ -42,12 +62,17 @@ public class TaxCalculator
 
 
         }
-
     }
+    #endregion
 
+
+
+
+    //this method will return value of calculated tax
+    #region Taxcalculation Method
     public double CalculateTax()
     {
-        if (this._userAge <= 60)
+        if (this._userAge <= _ageLimitforfFirstCategory)
         {
             if (this._userSalary <= 250000)
             {
@@ -71,7 +96,7 @@ public class TaxCalculator
             }
             return this._taxAmount;
         }
-        if (this._userAge > 60 && this._userAge <= 80)
+        if (this._userAge > _ageLimitforfFirstCategory && this._userAge <= _ageLimitforSecondgCategory)
         {
             if (this._userSalary <= 300000)
             {
@@ -92,7 +117,7 @@ public class TaxCalculator
             }
             return this._taxAmount;
         }
-        if (this._userAge > 80)
+        if (this._userAge > _ageLimitforSecondgCategory)
         {
             if (this._userSalary <= 500000)
             {
@@ -115,13 +140,16 @@ public class TaxCalculator
         }
         return this._taxAmount;
     }
+    #endregion
 }
+#endregion
 
-
+#region Business logic
 class TaxCalculatorAssignment
 {
     public static void Main(string[] args)
     {
+        //local variables to store input from user
         double inputSalary;
         int inputAge;
 
@@ -129,23 +157,25 @@ class TaxCalculatorAssignment
 
     ValueAssignment:
         try
-        { 
+        {
             Console.WriteLine("Enter Income");
+            //this will verify values passed by user or not\
             while (!double.TryParse(Console.ReadLine(), out inputSalary))
             {
 
                 throw new ArgumentException("Invalid value please enter valid Number");
             }
-        
-        
+
+
             Console.WriteLine("Enter Age");
             while (!int.TryParse(Console.ReadLine(), out inputAge))
             {
                 throw new ArgumentException("Invalid value please enter valid Number");
             }
-        
-        
 
+
+
+            //object initializaon section
             TaxCalculator firstcal = new TaxCalculator(inputSalary, inputAge);
             double result = firstcal.CalculateTax();
             Console.WriteLine("===================================");
@@ -162,3 +192,4 @@ class TaxCalculatorAssignment
 
     }
 }
+#endregion
